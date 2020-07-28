bd53bdd0a346efde1e988213b4145489c1627b2d
Johnny Young <youngerblack@gmail.com>
2020-07-27 22:19:26 -0500

    Initial commit

---
+++ app/src/main/java/com/young/cursokoltinantonioleiva/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.young.cursokoltinantonioleiva
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toast("Mi primera función jejejeje...")
+
+        var tvMessage: EditText = findViewById(R.id.etmessage)
+        var button:Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            toast("Hola ${tvMessage.text}")
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/young/cursokoltinantonioleiva/funciones.kt
@@ -0,0 +1,10 @@
+package com.young.cursokoltinantonioleiva
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast (message: String){
+
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+}
